2c897804f652c522102b57fb98cbd81bd1e265ee
diogofbrito <diogobrito74@gmail.com>
2026-01-08 14:31:55 +0000

    feat: new

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,8 +1,9 @@
 import projetos from './projetos'
 import about from './about'
 import carouselHome from './carouselHome'
+import carouselHomeMobile from './carouselHomeMobile'
 import blockContent from './blockContent'
 import press from './press'
 import imaisd from './imaisd'
 
-export const schemaTypes = [projetos, imaisd,about, press, carouselHome, blockContent]
+export const schemaTypes = [projetos, imaisd, about, press, carouselHome, carouselHomeMobile,blockContent]
